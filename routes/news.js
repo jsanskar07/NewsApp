04654ed1fb9140326e70857e60346f0d632404b6
jsanskar07 <jsanskar007@gmail.com>
2022-01-27 16:51:33 +0530

    first commit

---
+++ routes/news.js
@@ -0,0 +1,63 @@
+
+const express = require('express')
+const axios = require('axios')
+const newsr=express.Router()
+const moment = require('moment')
+const math = require('math')
+
+
+newsr.get('/',async(req,res)=>{
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=in&' +
+          'apiKey=89dd2ad6f9b44b92bd702b820a4388ae';
+
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
+})
+
+newsr.post('/search',async(req,res)=>{
+    const search=req.body.search
+    //console.log(req.body.search)
+    try {        
+        const url = `https://newsapi.org/v2/everything?q=${search}&from=2022-01-27&sortBy=popularity&apiKey=89dd2ad6f9b44b92bd702b820a4388ae`;
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+    }
+})
+
+newsr.get('/news/:category',async(req,res)=>{
+    var category = req.params.category;
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?country=in&category=' + category + '&apiKey=89dd2ad6f9b44b92bd702b820a4388ae';
+
+        const news_get =await axios.get(url)
+        res.render('category',{articles:news_get.data.articles})
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
+})
+
+newsr.get('*',async(req,res)=>{
+    res.render('error');
+})
+
+newsr.get
+
+module.exports=newsr;
